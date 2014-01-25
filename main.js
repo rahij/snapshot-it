c85defa9d3c6d18ae4c7200445bebba541f55b49
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2014-01-26 01:48:26 +0800

    I dont even remember the changes. tl;dr: new features/bug fixes/UI improvements

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ var fs = require('fs');
 var mime = require('mime');
 var shell = require('nw.gui').Shell;
 var jade = require('jade');
+exec = require('child_process').exec;
 
 global.current_repo_path = "";
 global.current_repo = null;
+global.current_rev  = "";
 
 $(document).ready(function() {
   $(".navbutton").click(function(){
@@ -58,8 +60,29 @@ $(document).ready(function() {
 });
 
 var format_commits = jade.compile([
+    'h5 Unsaved Changes',
+    // '#diff !{commits.diff}',
+    '#commits',
     '- each commit in commits',
-    '  .commit',
-    '    .commit-desc #{commit.message}',
-    '    button.revert-button.btn(data-id="#{commit.id}") Revert to this version'
+    '   .commit',
+    '     span.commit-desc #{commit.message}',
+    '     if commits.head == commit.id',
+    '       span.label.label-success Current Version',
+    '     .help-buttons-wrapper',
+    '       button.diff-button.btn.btn-primary(data-id="#{commit.id}") Click to view changes',
+    '       button.revert-button.btn.btn-danger(data-id="#{commit.id}") Revert to this version'
   ].join('\n'));
+
+function render_commits(){
+  global.current_repo.commits(function(err, commits) {
+    var bash = "git rev-parse HEAD";
+    exec(bash, {cwd: global.current_repo_path}, function(error, stdout, stderr){
+      global.current_rev  = stdout.replace(/^\s+|\s+$/g, '');
+      commits['head'] = global.current_rev;
+      exec("git diff", {cwd: global.current_repo_path}, function(error, stdout, stderr){
+        commits['diff'] = stdout.replace(/\n/g, '<br />');;
+        $("#display_area").html(format_commits({"commits" : commits}));
+      });
+    });
+  });
+}
